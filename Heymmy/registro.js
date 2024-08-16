cfc1ce35793a0cd7cc361e32ae97bac045fa3ba3
heymjzz05 <161257721+heymjzz05@users.noreply.github.com>
2024-08-16 17:17:21 -0600

    Se modifican pantallas de Registro, Foro y Consultar
    Se aplica cambio en registro que permita seleccionar tipo de usuario, en Foro, se agregan boton de like y dislike, ademas de poder agregar publicaciones, en Consultar, se editan campos de tabla a mostrar

--- Heymmy/registro.js
+++ Heymmy/registro.js
@@ -1,29 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('registerForm');
-    
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Evitar el envío del formulario
+  const form = document.getElementById('registerForm');
   
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Evitar el envío del formulario
+
       // Obtener datos del formulario
       const nombre = document.getElementById('nombre').value;
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
-  
+      const tipoUsuario = document.getElementById('tipoUsuario').value; // Obtener el valor del selector
+
       // Obtener datos guardados en localStorage
       let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-  
+
       // Añadir el nuevo usuario
       usuarios.push({
-        nombre: nombre,
-        email: email,
-        password: password
+          nombre: nombre,
+          email: email,
+          password: password,
+          tipoUsuario: tipoUsuario // Agregar el tipo de usuario
       });
-  
+
       // Guardar datos en localStorage
       localStorage.setItem('usuarios', JSON.stringify(usuarios));
-  
+
       // Redirigir a index.html
       window.location.href = 'index.html';
-    });
   });
-  
+});
