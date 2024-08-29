3ef99e933c9188e9f5dc19e63e228e97a9e48873
Kevin R. <kramirez8808@gmail.com>
2024-08-29 16:48:33 -0600

    minor fix

--- web-001/View/html-kevin/soporte.php
+++ web-001/View/html-kevin/soporte.php
@@ -12,13 +12,6 @@
             break;
       }
     }
-    
-  //   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  //     echo '<pre>';
-  //     print_r($_POST);
-  //     echo '</pre>';
-  //  }
-  
 
 ?>
 
